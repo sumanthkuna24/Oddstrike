47d91f6393112b11d3d66109b5f94666fcc8807b
sumanthkuna24 <sumanthkuna508@gmail.com>
2026-02-19 01:10:38 +0530

    Remove node_modules and add gitignore

--- oddstrike-client/src/socket.js
+++ oddstrike-client/src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://oddstrike-server.onrender.com");
+const socket = io("https://oddstrike-server.onrender.com", {
+  transports: ["polling", "websocket"],
+  withCredentials: true
+});
 
 export default socket;
